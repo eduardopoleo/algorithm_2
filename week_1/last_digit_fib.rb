798161fa15f4eb4cafd609ca4708d87920ee5e1b
Eduardo Poleo <dev+eduardo@influitive.com>
2016-11-10 06:40:14 -0500

    Small fib pass

---
+++ week_1/last_digit_fib.rb
@@ -0,0 +1,18 @@
+input = STDIN.gets.chomp
+input = input.to_i
+
+def fib(n)
+  return 0 if n == 0
+  return 1 if n == 1
+
+  sequence = [0, 1]
+
+  for i in 2..n
+    number = (sequence[i-1] + sequence[i-2])
+    sequence << number
+  end
+
+  number % 10
+end
+
+STDOUT.write fib(input)
