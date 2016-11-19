3e0d49de2f1c9de568d9d7e30de5bf0a82a5c5e8
Eduardo Poleo <dev+eduardo@influitive.com>
2016-11-19 14:04:39 -0500

    Assignment passed

--- week_3/Notes/knapsack.rb
+++ week_3/Notes/knapsack.rb
@@ -3,28 +3,18 @@ items = [[2, 14], [3, 18], [4, 20]]
 # weight_available = current weight available in the knapsack
 
 def knapsack(weight_available, items)
-# this will be in the same order as the items that were introduced
 	items_packed = [0, 0, 0]
-	# total value already packed in the sack
 	total_value = 0
 	n = items.length
 
-# basically looped through all the elements in items
 	for i in 0..n
-	# if there is not more weight available in the sack return the current values
 		if weight_available == 0
 			return [total_value, items_packed] 		
 		end
-# this routine returns the index of the current most valuable item
-# I guess we could have the array sorted so that we can avoid doing this.
 		index = choose_max_value(items)
-# get information for that item
 		item = items[index]
-# choose the minimun between the outstanding quantity of that item and the 
-# and the outstanding space in the sack
 		a = [item[0], weight_available].min
 
-# update the values accordingly 
 		total_value = total_value + a * item[1]/item[0] # total value in the sack
 		item[1] = item[1] - a # substrack the amount in the items
 		items_packed[index] = items_packed[index] + a # updates the items packed in the list
--- week_3/exercises/max_loot.rb
+++ week_3/exercises/max_loot.rb
@@ -1,17 +1,40 @@
-require 'pry'
+# require 'pry'
 input = STDIN.readlines
 constrains = input.first.chomp.split(" ")
-N = constrains[0].to_i
-W = constrains[1].to_i
+N = constrains[0].to_f
+W = constrains[1].to_f
 
 items = []
-
+items = items.sort_by { |item| item[1] }
 for i in 1...input.length 
-	item = input[i].chomp.split(" ").map(&:to_i)
+	item = input[i].chomp.split(" ").map(&:to_f)
+	ratio = item[0] / item[1] 
+	item << ratio
 	items << item
 end
 
+# [ value, weight, ratio]
+items = items.sort_by {|item| item[2]}.reverse
+
 def max_loot(w, items)
+	total_value = 0.to_f
+
+	items.each do |item|
+		value = item[0]
+		weight = item[1]
+		
+		if weight <= w
+			total_value += value
+			w -= weight
+		else
+			w_ratio = w / weight
+			new_value = value * w_ratio
+			total_value += new_value
+			break
+		end
+	end	
+
+	total_value.round(4)
 end
 
 STDOUT.write max_loot(W, items)
